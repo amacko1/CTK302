8b56ddc3f6d157d779355fc7768d1429785821dc
amacko1 <77295359+amacko1@users.noreply.github.com>
2021-02-24 09:27:29 -0600

    boom

--- Unit1/fonts/sketch.js
+++ Unit1/fonts/sketch.js
@@ -2,20 +2,20 @@ let f1;
 let f2;
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(500, 500) ;
   textAlign(CENTER)
 
-  f1 = loadFont("assets/Myrlissa.otf");
-  f2 = loadFont("assets/Winter.ttf");
+  f1 = loadFont("assets/Winter.ttf") ;
+  f2 = loadFont("assets/Sansation.ttf") ;
 }
 
 function draw() {
-  background(100);
-textSize (48);
+  background(100) ;
+  textSize (48) ;
 
 textFont(f1);
-text("hello", width/2, height/2);
+text("hello", width/2, height/2) ;
 
-textFont(f2, 32);
-text("world", width/2, height/ 2 + 50);
+textFont(f2, 32) ;
+text("world", width/2, height/ 2 + 50) ;
 }
---
+++ Unit2/state/sketch.js
@@ -0,0 +1,47 @@
+let state = 0 ;
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+
+  switch(state) {
+    case 0:
+    background('grey') ;
+    text("state 0", 100, 100) ;
+    for (let j = 0; j < 20; j++) {
+      for (let i = 0; i < 20; i++) {
+        rect(i * 50 + 10, j * 50 + 10, 25, 5);
+      }
+    }
+    break ;
+
+    case 1:
+    background('red') ;
+    text("state 1", 100, 100) ;
+    break ;
+
+    case 2:
+    background('green') ;
+    text("state 2", 100, 100) ;
+    break ;
+
+    case 3:
+    background('yellow') ;
+    text("state 3", 100, 100) ;
+    break ;
+
+    case 4:
+    background('blue') ;
+    text("state 4", 100, 100) ;
+    break ;
+  }
+}
+
+function mouseReleased() {
+  state++ ;
+  if (state > 4) {
+    state = 0 ;
+  }
+}

6ab536b7fb182e173d946eb69dc235b9bd0ac74a
amacko1 <77295359+amacko1@users.noreply.github.com>
2021-02-28 21:13:33 -0600

    add next

--- Unit2/mp2test/sketch.js
+++ Unit2/mp2test/sketch.js
@@ -1,144 +1,199 @@
-let state = 0 ;
+let state = 0;
+let myTimer = 0;
+let song1, song2;
+
+function preload() {
+  song1 = loadSound("assets/gelo.mp3");
+  song2 = loadSound("assets/melt.mp3");
 
 
+  song1.loop();
+  song1.pause();
+  song2.loop();
+  song2.pause();
+
+}
 
 function setup() {
   createCanvas(500, 700);
-	background(41, 170, 255);
+  background(41, 170, 255);
   fill(0, 142, 188);
-	strokeWeight(0);
-	rect(0, 500, width, 200);
-
-
-	}
+  strokeWeight(0);
+  rect(0, 500, width, 200);
+}
 
 function draw() {
-
-  switch(state) {
+print(state);
+  switch (state) {
     case 0:
-    background(41, 170, 255);
-    fill(0, 142, 188);
-  	strokeWeight(0);
-  	rect(0, 500, width, 200);
-
-
-    //snowman
-  	fill(255);
-  	ellipse(250, 500, 200, 200);
-  	ellipse(250, 375, 150, 150);
-  	ellipse(250, 275, 100, 100);
-
-  	//hat
-  	fill(0);
-  	rect(215, 140, 70, 100);
-  	ellipse(250, 240, 100, 10);
-
-  	//eyes
-  	ellipse(235, 265, 5, 5);
-  	ellipse(265, 265, 5, 5);
-
-  	//carrot
-  	fill(255, 165, 0);
-  	triangle(250, 275, 250, 285, 290, 300);
-
-  	//arms
-  	stroke(139, 69, 19);
-  	strokeWeight(5);
-  	line(180, 375, 100, 350);
-  	line(320, 375, 400, 350);
-    strokeWeight(0);
-    fill('black')
-    text("The snowman on a snowy day ", 300, 100);
-
-
-    break ;
+      //song1.play();
+      state = 1;
+      break;
 
     case 1:
+      myTimer++;
+      if (myTimer > 7 * 60) {
+        myTimer = 0;
+        song1.pause();
+        state = 2;
+      }
 
-    background(41, 170, 255);
-    fill(0, 142, 188);
-  	strokeWeight(0);
-  	rect(0, 500, width, 200);
-    //snowman
-  	fill(255);
-  	ellipse(250, 500, 200, 200);
-  	ellipse(250, 375, 150, 150);
-  	ellipse(250, 275, 100, 100);
-
-  	//hat
-  	fill(0);
-  	rect(215, 140, 70, 100);
-  	ellipse(250, 240, 100, 10);
-
-  	//eyes
-  	ellipse(235, 265, 5, 5);
-  	ellipse(265, 265, 5, 5);
-
-  	//carrot
-  	fill(255, 165, 0);
-  	triangle(250, 275, 250, 285, 290, 300);
-
-  	//arms
-  	stroke(139, 69, 19);
-  	strokeWeight(5);
-    line(180, 375, 100, 350);
-  	line(320, 375, 400, 350);
-    strokeWeight(0);
-    fill('black')
-    text("but the sun came up", 300, 100);
-
-
-      //sun
-      fill('yellow');
-      ellipse(56, -10, 300, 300);
-
-    break ;
+      snow1();
+      break;
 
     case 2:
-    background(41, 170, 255);
-    fill(0, 142, 188);
-  	strokeWeight(0);
-  	rect(0, 500, width, 200);
-    //snowman
-  	fill(255);
-    ellipse(250, 600, 200, 120);
-  	ellipse(250, 570, 150, 150);
-  	ellipse(250, 520, 100, 100);
+      //song1.pause();
+      song1.play();
+      state = 3;
+      break;
 
-  	//hat
-  	fill(0);
-  	rect(215, 520, 70, 100);
-  	ellipse(250, 620, 100, 10);
+    case 3:
+      myTimer++;
+      if (myTimer > 5 * 60) {
+        myTimer = 0;
+        state = 4;
+        song1.pause();
+      }
 
-  	//eyes
-  	ellipse(235, 500, 5, 5);
-  	ellipse(265, 500, 5, 5);
+      snowsun();
 
-  	//carrot
-  	fill(255, 165, 0);
-  	triangle(250, 390, 250, 390, 290, 300);
+      break;
 
-  	//arms
-  	stroke(139, 69, 19);
-  	strokeWeight(5);
-    line(180, 500, 50, 500);
-  	line(320, 510, 400, 500);
-    strokeWeight(0);
-    fill('black')
-    text("BYE BYE snowman!!!!", 300, 100);
+    case 4:
+      myTimer++;
+      if (myTimer > 7 * 60) {
+        myTimer = 0;
+        state = 0;
+        song2.pause();
+      }
+
+      snowmelt();
 
 
-
-      //sun
-      fill('yellow');
-      ellipse(56, -10, 300, 300);
-
-    break ;
+      break;
   }
 }
 
 
 function mouseReleased() {
-  state++ ;
-  if (state > 3) state = 0 ;
+  song1.pause();
+  song2.pause();
+
+  state++;
+  if (state > 4) state = 0;
+
+}
+
+function snow1() {
+  background(41, 170, 255);
+  fill(0, 142, 188);
+  strokeWeight(0);
+  rect(0, 500, width, 200);
+
+
+  //snowman
+  fill(255);
+  ellipse(250, 500, 200, 200);
+  ellipse(250, 375, 150, 150);
+  ellipse(250, 275, 100, 100);
+
+  //hat
+  fill(0);
+  rect(215, 140, 70, 100);
+  ellipse(250, 240, 100, 10);
+
+  //eyes
+  ellipse(235, 265, 5, 5);
+  ellipse(265, 265, 5, 5);
+
+  //carrot
+  fill(255, 165, 0);
+  triangle(250, 275, 250, 285, 290, 300);
+
+  //arms
+  stroke(139, 69, 19);
+  strokeWeight(5);
+  line(180, 375, 100, 350);
+  line(320, 375, 400, 350);
+  strokeWeight(0);
+  fill('black')
+  text("The snowman on a snowy day ", 300, 100);
+}
+
+function snowsun() {
+  background(41, 170, 255);
+  fill(0, 142, 188);
+  strokeWeight(0);
+  rect(0, 500, width, 200);
+  //snowman
+  fill(255);
+  ellipse(250, 500, 200, 200);
+  ellipse(250, 375, 150, 150);
+  ellipse(250, 275, 100, 100);
+
+  //hat
+  fill(0);
+  rect(215, 140, 70, 100);
+  ellipse(250, 240, 100, 10);
+
+  //eyes
+  ellipse(235, 265, 5, 5);
+  ellipse(265, 265, 5, 5);
+
+  //carrot
+  fill(255, 165, 0);
+  triangle(250, 275, 250, 285, 290, 300);
+
+  //arms
+  stroke(139, 69, 19);
+  strokeWeight(5);
+  line(180, 375, 100, 350);
+  line(320, 375, 400, 350);
+  strokeWeight(0);
+  fill('black')
+  text("but the sun came up", 300, 100);
+
+  //sun
+  fill('yellow');
+  ellipse(56, -10, 300, 300);
+
+}
+
+function snowmelt() {
+  background(41, 170, 255);
+  fill(0, 142, 188);
+  strokeWeight(0);
+  rect(0, 500, width, 200);
+  //snowman
+  fill(255);
+  ellipse(250, 600, 200, 120);
+  ellipse(250, 570, 150, 150);
+  ellipse(250, 520, 100, 100);
+
+  //hat
+  fill(0);
+  rect(215, 520, 70, 100);
+  ellipse(250, 620, 100, 10);
+
+  //eyes
+  ellipse(235, 500, 5, 5);
+  ellipse(265, 500, 5, 5);
+
+  //carrot
+  fill(255, 165, 0);
+  triangle(250, 390, 250, 390, 290, 300);
+
+  //arms
+  stroke(139, 69, 19);
+  strokeWeight(5);
+  line(180, 500, 50, 500);
+  line(320, 510, 400, 500);
+  strokeWeight(0);
+  fill('black')
+  text("BYE BYE snowman!!!!", 300, 100);
+
+  //sun
+  fill('yellow');
+  ellipse(56, -10, 400, 400);
+
 }
